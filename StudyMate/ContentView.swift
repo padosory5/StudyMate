7e4c747db90d8d983e5f2380db94a245ed234f0a
padosory5 <padosory5@gmail.com>
2023-11-01 17:53:27 -0700

    Initial Commit

---
+++ StudyMate/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StudyMate
+//
+//  Created by 홍영준 on 11/1/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StudyMate/StudyMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudyMateApp.swift
+//  StudyMate
+//
+//  Created by 홍영준 on 11/1/23.
+//
+
+import SwiftUI
+
+@main
+struct StudyMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

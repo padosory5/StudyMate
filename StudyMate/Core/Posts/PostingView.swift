a5c888f42d3ad752e681fec34abebf482fc804d3
padosory5 <padosory5@gmail.com>
2023-11-01 22:07:43 -0700

    Added Files for each button

---
+++ StudyMate/Core/Posts/PostingView.swift
@@ -0,0 +1,18 @@
+//
+//  PostingView.swift
+//  StudyMate
+//
+//  Created by 홍영준 on 11/1/23.
+//
+
+import SwiftUI
+
+struct PostingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PostingView()
+}
---
+++ StudyMate/Core/Profile/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  StudyMate
+//
+//  Created by 홍영준 on 11/1/23.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileView()
+}

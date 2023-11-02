637fc181ad1b0f8508696b6b3d36a34735d0184f
padosory5 <padosory5@gmail.com>
2023-11-01 23:26:18 -0700

    Changed names

--- StudyMateUITests/StudyMateUITestsLaunchTests.swift
+++ StudyMateUITests/StudyMateUITestsLaunchTests.swift
@@ -7,7 +7,7 @@
 
 import XCTest
 
-final class StudyMate2UITestsLaunchTests: XCTestCase {
+final class StudyMateUITestsLaunchTests: XCTestCase {
 
     override class var runsForEachTargetApplicationUIConfiguration: Bool {
         true
